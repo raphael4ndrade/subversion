cd9e526c0949e4daf268243b5bebfc46bab598ef
YtalloGadelha <ytallolg@gmail.com>
2016-04-26 08:46:59 -0300

    enviando commit da branch Ytallo

--- src/ShowMessageInSVN.java
+++ src/ShowMessageInSVN.java
@@ -3,7 +3,7 @@ public class ShowMessageInSVN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Branch ytallo OK");
+		System.out.println("enviando branch ytallo ");
 
 	}
 
